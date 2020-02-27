59347a0002aafbd13aafd0d14ca286452ac6cd5f
maycon-lnx <56367704+maycon-lnx@users.noreply.github.com>
2020-02-27 19:53:51 -0300

    Update xmlc.php

--- xmlc.php
+++ xmlc.php
@@ -19,16 +19,11 @@ charset=iso-8859-1" />
     //faz o loop nas tag com o nome "item"
         //exibe o valor das tags que estão dentro da tag "item"
         //utilizamos a função "utf8_decode" para exibir os caracteres corretamente
-        echo "<strong>Título:</strong> "
-        .utf8_decode($item -> title)."<br />";
-        echo "<strong>Link:</strong> "
+
         .utf8_decode($item -> link)."<br />";
         echo "<strong>Descrição:</strong> "
         .utf8_decode($item -> description)."<br />";
-        echo "<strong>Autor:</strong> "
-        .utf8_decode($item -> author)."<br />";
-        echo "<strong>Data:</strong> "
-        .utf8_decode($item -> pubDate)."<br />";
+
         echo "<br />";
     } //fim do foreach
 ?>
